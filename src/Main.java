7aa420eea7df82343be443b01d987a968b486948
marrakechy <belmarrakechy21@coe.edu>
2023-01-28 16:37:46 -0600

    Update 4

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@
 public class Main {
     public static void main(String[] args) {
         Timer timer = new Timer(1, 30, 45);
+
         System.out.println(timer);
+
         timer.tick();
+
         System.out.println(timer);
+
         timer.addTime(30);
+
         System.out.println(timer);
     }
 }
